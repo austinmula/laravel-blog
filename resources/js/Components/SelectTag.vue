0d39471b995c89229e8f502bdcebb900291b0bfa
“austinmula” <maustin64.ma@gmail.com>
2023-07-26 15:56:49 +0300

    complete inputs for blog details

---
+++ resources/js/Components/SelectTag.vue
@@ -0,0 +1,48 @@
+<template>
+    <vue3-tags-input
+        v-model:tags="tags"
+        v-model="tag"
+        :select="true"
+        :select-items="selectItems"
+        @on-select="handleSelectedTag"
+        @on-tags-changed="handleChangeTag"
+        placeholder="Select the tag"
+    >
+        <template #item="{ tag, index }">
+            {{ tag.text }}
+        </template>
+        <template #no-data> No Data </template>
+        <template #select-item="tag">
+            {{ tag.text }}
+        </template>
+    </vue3-tags-input>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Vue3TagsInput from "vue3-tags-input";
+
+export default defineComponent({
+    name: "SlotTemplate",
+
+    components: {
+        Vue3TagsInput,
+    },
+
+    data() {
+        return {
+            tags: [],
+            tag: "",
+            selectItems: [{ text: "HTML" }, { text: "CSS" }, { text: "VUE" }],
+        };
+    },
+    methods: {
+        handleSelectedTag(tag) {
+            this.tags.push(tag);
+        },
+        handleChangeTag(tags) {
+            this.tags = tags;
+        },
+    },
+});
+</script>
--- resources/js/Pages/Dashboard/Blog/CreateBlog.vue
+++ resources/js/Pages/Dashboard/Blog/CreateBlog.vue
@@ -1,8 +1,17 @@
 <script>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
+import SelectTag from "@/Components/SelectTag.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 export default {
     layout: AdminLayout,
+    components: {
+        SelectTag,
+        PrimaryButton,
+        QuillEditor,
+    },
 };
 </script>
 
@@ -19,9 +28,13 @@ export default {
             </p>
             <PrimaryButton>Preview</PrimaryButton>
         </div>
-
         <form>
-            <div class="flex flex-col gap-5 mb-5">
+            <div
+                class="flex flex-col gap-5 mb-5 bg-slate-50 p-4 rounded-md shadow-md"
+            >
+                <h1 class="text-xl font-semibold whitespace-nowrap">
+                    Blog Details
+                </h1>
                 <div class="form-control">
                     <label
                         class="text-sm text-gray-700 font-semibold block mb-1"
@@ -48,9 +61,57 @@ export default {
                     />
                 </div>
 
-                <Vue3TagsInput :tags="tags" placeholder="input tags" />
+                <div class="form-control">
+                    <label
+                        class="text-sm text-gray-700 font-semibold block mb-1"
+                        >Blog Tags</label
+                    >
+
+                    <SelectTag />
+                </div>
+
+                <div class="form-control">
+                    <label
+                        class="text-sm text-gray-700 font-semibold block mb-1"
+                        >Blog Summary</label
+                    >
+
+                    <textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        class="w-full border-gray-400 rounded-md resize-none"
+                    ></textarea>
+                </div>
             </div>
+            <h1 class="text-xl font-semibold whitespace-nowrap mb-5 mt-9">
+                Blog Content
+            </h1>
+            <QuillEditor toolbar="full" />
         </form>
         <!-- <div class="space-y-6 md:space-x-2 md:space-y-0"></div> -->
     </div>
 </template>
+
+<style lang="css">
+.v3ti {
+    padding-block: 5px;
+}
+.v3ti .v3ti-tag {
+    background: #4a3a92;
+}
+
+.v3ti .v3ti-tag .v3ti-remove-tag {
+    color: #000000;
+    transition: color 0.3s;
+}
+
+.v3ti .v3ti-tag .v3ti-remove-tag:hover {
+    color: #ffffff;
+}
+
+.ql-toolbar.ql-snow + .ql-container.ql-snow {
+    height: 400px;
+}
+</style>
