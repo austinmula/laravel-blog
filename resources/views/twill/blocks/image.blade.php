d385813401cc15ad0109baea20cfaf8fae420cdd
“austinmula” <maustin64.ma@gmail.com>
2023-10-03 14:49:00 +0300

    fixing the image block, was showing txt instead

--- resources/views/twill/blocks/image.blade.php
+++ resources/views/twill/blocks/image.blade.php
@@ -2,25 +2,4 @@
 @twillBlockIcon('text')
 @twillBlockGroup('app')
 
-<x-twill::input
-    name="title"
-    label="Title"
-    :translated="true"
-/>
-
-<x-twill::wysiwyg
-    name="text"
-    label="Text"
-    placeholder="Text"
-    :toolbar-options="[
-        'bold',
-        'italic',
-        ['list' => 'bullet'],
-        ['list' => 'ordered'],
-        [ 'script' => 'super' ],
-        [ 'script' => 'sub' ],
-        'link',
-        'clean'
-    ]"
-    :translated="true"
-/>
+<x-twill::medias name="highlight" label="Highlight" />
